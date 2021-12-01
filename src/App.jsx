dad0a1eefcbbd0735cd8496e82b588ba16c05ddc
Jonathan Mwaniki <idolearndaily.gmai.com>
2021-12-01 12:58:51 +0300

    added the complete version

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
 import './App.css';
+import {Link}  from 'react-router-dom'
 
 
 function App() {
     return ( 
         <div className = "App" >
-            <h1> Bookkeeper </h1> 
+            <h1> Bookkeeper </h1>
+            <nav>
+                <Link to="/invoices">Invoices</Link>
+                <Link to="/expenses">Expenses</Link>
+            </nav>
             
         </div>
     );
